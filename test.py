ad97eb185beff7ab33363ade34c29815f780ac3c
LNDMN <147999671+LNDMN@users.noreply.github.com>
2024-04-30 13:54:25 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-echo "# ULTRAPI" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add github https://github.com/LNDMN/ULTRAPI.git
-git push -u github main
+
